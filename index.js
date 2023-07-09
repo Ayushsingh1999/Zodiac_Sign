68c4a6ba2102cc8cc85300c2e3b97f237df24cc9
unknown <ayushsingh842018@gmail.com>
2023-07-10 01:29:07 +0530

    this is first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const zodiac = document.getElementById("zodiac");
+
+const body = document.body;
+
+const ChangeBackground =()=>
+{
+    switch (zodiac.value) {
+        case "aries": body.style.backgroundColor ="#fc0303";
+            break;  
+         case "tarus": body.style.backgroundColor ="#03fc6f";
+         break;
+         case "gemini": body.style.backgroundColor ="#fce514";
+         break;
+         case "cancer": body.style.backgroundColor ="#fffdd0";
+         break;
+         case "leo": body.style.backgroundColor ="#fcba03";
+         break; 
+         case "virgo": body.style.backgroundColor ="#40231b";
+        break;
+        case "libra": body.style.backgroundColor ="#9fd9fc";
+        break;
+        case "scorpio": body.style.backgroundColor ="#1f1f1f";
+        break;
+        case "sagittarius": body.style.backgroundColor ="#994ead";
+        break;  
+        case "aquarius": body.style.backgroundColor ="#579eeb";
+        break; 
+        case "capricorn": body.style.backgroundColor ="#eb8d57";
+        break;
+        case "pisces": body.style.backgroundColor ="#fc88d8";
+        break;
+        
+    
+        default:
+            break;
+    }
+}
